8e2a4df8aa422110a7f8e186451a9b8f8987e419
akash shevgan <akashshevgan11@gmail.com>
2021-04-06 08:30:31 +0530

    using switch case

---
+++ empWageComp.sh
@@ -0,0 +1,17 @@
+isparttime=1
+isfulltime=2
+emprateperhr=20
+randomCheck=$((RANDOM%3));
+case $randomCheck in
+	$isfulltime)
+		empprh=8
+		;;
+	$isparttime)
+		empprh=4
+		;;
+	*)
+	empprh=0
+		;;
+esac
+salary=$(($empprh*emprateperhr));
+
